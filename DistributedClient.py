5af81759750cf872ddec7c0e44fb6f2d9c7c5974
GeoGalvanic <gqs@case.edu>
2021-05-25 20:01:53 +0200

    List Creator and Even Calculator Functions

--- DistributedClient.py
+++ DistributedClient.py
@@ -1,24 +1,56 @@
+######
 #
-#   Hello World client in Python
-#   Connects REQ socket to tcp://localhost:5555
-#   Sends "Hello" to server, expects "World" back
+# Client-Side script that gathers tasks from server and distributes them for processing to the cores on the client machine
 #
+#####
 
 import zmq
+from random import randrange
+from time import sleep
+
+#Functions that we will want to run once connected to server
+def createList():
+    list = []
+    for number in range(10):
+        list.append(randrange(10))
+    return list
+
+def evenCalculator(i):
+    if i %2 == 0:
+        return True
+    else:
+        return False
 
 context = zmq.Context()
 
-#  Socket to talk to server
-print("Connecting to hello world server...")
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://10.0.2.2:5555")
+#  Sockets to talk to server
+print("Connecting to server...")
+receiver = context.socket(zmq.PULL)
+receiver.connect("tcp://10.0.2.2:5555")
 
-#  Do 10 requests, waiting each time for a response
-for request in range(10):
-    print(f"Sending request {request} ...")
-    socket.send_string("Hello")
+sender = context.socket(zmq.PUSH)
+sender.connect("tcp://10.0.2.2:5555")
 
-    #  Get the reply.
-    message = socket.recv()
-    print(f"Received reply {request} [ {message.decode()} ]")
+while True:
+    #Collect tasks from the server
+    s = receiver.recv()
 
+    # Simple progress indicator for the viewer
+    sys.stdout.write('.')
+    sys.stdout.flush()
+
+    print(f"Received {s} from the server.")
+    #Determine which task should be run
+    if s[0] == "createList":
+        sender.send(createList)
+        print("Successfully sent task list.") 
+    elif s[0] == "evenCalculator":
+        sender.send(evenCalculator(s[1]))
+        print("Sent evenCalculator return.")
+    elif s[0] == "wait":
+        sleep(5)
+        continue
+    elif s[0] == "stop":
+        break
+
+print("All tasks completed successfully...")
--- DistributedServer.py
+++ DistributedServer.py
@@ -1,23 +1,43 @@
+#####
 #
-#   Hello World server in Python
-#   Binds REP socket to tcp://*:5555
-#   Expects b"Hello" from client, replies with b"World"
+# Server-Side task distributor
 #
+#####
 
 import time
 import zmq
 
+print(f"Start: {time.time}")
+
 context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
 
-while True:
-    #  Wait for next request from client
-    message = socket.recv()
-    print(f"Received request: {message.decode()}")
+#Sockets to talk with workers
+sender = context.socket(zmq.PUSH)
+sender.bind("tcp://*:5555")
 
-    #  Do some 'work'
-    time.sleep(1)
+receiver = context.socket(zmq.PULL)
+receiver.bind("tcp://*:5556")
 
-    #  Send reply back to client
-    socket.send_string("World")
+#Send and receive the tasklist from first worker
+sender.send(["createList"])
+taskList = reciever.recv()
+print(f"Returned tasklist: {taskList}")
+taskLen = len(taskList)
+
+#send all tasks to works
+while taskList:
+    sender.send(["evenCalculator",taskList.pop(0)])
+
+#process the returned results
+numEven = 0
+for taskNum in range(taskLen):
+    s = receiver.recv()
+
+    if s == True:
+        numEven += 1
+
+print(f"Found {numEven} even numbers")
+
+sender.send(["stop"])
+
+print(f"Script completed at {time.time}")
